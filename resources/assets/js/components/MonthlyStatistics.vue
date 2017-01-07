9bdafbffcf82723b668873171c000c5ac41d5149
Xdega121 <xdega121@gmail.com>
2017-01-07 15:52:55 -0600

    CSS Styling for Statistics Panels.

--- resources/assets/js/components/MonthlyStatistics.vue
+++ resources/assets/js/components/MonthlyStatistics.vue
@@ -8,9 +8,23 @@
             <i v-show="loading" id="preloader" class="fa fa-spinner fa-pulse"></i>
 
             <div v-show="!loading" class="panel-body text-left">
-                <p><span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.encounters_current_month }}</span> Encounters</p>
-                <p><span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.actions_current_month }}</span> Actions</p>
-                <p><span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.duration_current_month }}</span> Minutes Served</p>
+
+                <div>
+                    <span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.encounters_current_month }}</span> 
+                    <span class="stat-label">Encounters</span>
+                </div>
+
+                <div>
+                    <span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.actions_current_month }}</span> 
+                    <span class="stat-label">Actions</span>
+                </div>
+
+                
+                <div>
+                    <span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.duration_current_month }}</span> 
+                    <span class="stat-label">Minutes Served</span>
+                </div>
+
             </div>
         </div>
     </div>
--- resources/assets/js/components/OverallStatistics.vue
+++ resources/assets/js/components/OverallStatistics.vue
@@ -8,10 +8,27 @@
             <i v-show="loading" id="preloader" class="fa fa-spinner fa-pulse"></i>
 
             <div v-show="!loading" class="panel-body text-left">
-                <p><span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.encounters_total }}</span> Encounters</p>
-                <p><span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.actions_total }}</span> Actions</p>
-                <p><span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.clients_total }}</span> Unique Clients</p>
-                <p><span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.duration_total }}</span> Minutes Served</p>
+                
+                <div>
+                    <span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.encounters_total }}</span> 
+                    <span class="stat-label">Encounters</span>
+                </div>
+
+                <div>
+                    <span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.actions_total }}</span> 
+                    <span class="stat-label">Actions</span>
+                </div>
+
+                <div>
+                    <span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.clients_total }}</span> 
+                    <span class="stat-label">Unique Clients</span>
+                </div>
+                
+                <div>
+                    <span class="big-number"><i v-show="loading" style="font-size: 40px;" class="fa fa-spinner fa-pulse"></i>{{ meta.duration_total }}</span>
+                    <span class="stat-label">Minutes Served</span>
+                </div>
+
             </div>
         </div>
     </div>
